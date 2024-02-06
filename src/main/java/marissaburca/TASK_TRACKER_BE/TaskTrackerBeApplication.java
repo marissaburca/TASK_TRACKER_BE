3a39339859837831c5aff7684b3e057f7bb1f1e7
Marissa Burca <Marissaburca@gmail.com>
2024-02-06 17:52:24 +0100

    Initial commit

---
+++ src/main/java/marissaburca/TASK_TRACKER_BE/TaskTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package marissaburca.TASK_TRACKER_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marissaburca/TASK_TRACKER_BE/TaskTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package marissaburca.TASK_TRACKER_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
